ea40b4a86b8e1df0edac9f94ed98e6320298d410
Andre Harper <harperandre277@gmail.com>
2025-09-18 14:04:06 -0400

    changed vite-config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
-// vite.config.js
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'primevue': fileURLToPath(new URL('./node_modules/primevue', import.meta.url)),
+      'primeicons': fileURLToPath(new URL('./node_modules/primeicons', import.meta.url)),
     },
   },
-})
+});
